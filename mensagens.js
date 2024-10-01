23e1a0b3b60d0e16b5508c52fda45e0226b2edc2
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-01 19:31:22 -0300

    Finalizado

--- mensagens.js
+++ mensagens.js
@@ -1,140 +1,161 @@
-
-const Descricao = [
+let marcadores = [
+    marker = {
+        latitude: -20.463218,
+        longitude: -54.613142,
+        imagem: "./img/MS/CampoGrande.png",
+        titulo: "CampoGrande",
+        descricao:`Campo Grande é a capital do estado de Mato Grosso do Sul, 
+        na Região Centro-Oeste do Brasil. Possui clima tropical,
+        com verões quentes e chuvosos e inverno seco.
+        Seu relevo é predominantemente planáltico.`,
+        area:"Área total:8.096 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-20.463218, -54.613142",
+        Habitantes:"Quantidade de Habitantes:954.537",
+        estado:"Estado:Mato Grosso do Sul",
+        prefeitura:"https://www.campogrande.ms.gov.br/"
+    },
+    marker = {
+        latitude: -20.463001,
+        longitude: -55.779558,
+        imagem: "./img/MS/Aquidauana.png",
+        titulo: "Aquidauana",
+        descricao:`Fundada em 15 de agosto de1892 por 05 pecuaristas,
+        é uma cidade centenária, possui uma arquitetura colonial
+        no Centro Histórico, às margens do rio Aquidauana
+        e entorno da Igreja Matriz, alguns prédios são
+        e outros estão em vias de se tornarem centenários.    `,
+        area:"Área total:17.008,5 Km²",
+        LatitudeeLongitude:"Latitude e Longitude:-20.463001, -55.779558",
+        Habitantes:"Quantidade de Habitantes:47 871",
+        estado:"Estado:Mato Grosso do Sul",
+        prefeitura:"https://www.aquidauana.ms.gov.br/"
+    },
+    marker = {
+        latitude: -20.788476,
+        longitude: -51.703229,
+        imagem: "./img/MS/TresLagoas.png",
+        titulo: "Três Lagoas",
+        descricao:`O município de Três Lagoas está localizado na 
+        região leste de Mato Grosso do Sul. A cidade 
+        foi fundada em 15 de junho de 1915 e é 
+        considerada o terceiro município mais 
+        populoso do Estado com aproximadamente 
+        125 mil habitantes segundo população 
+        estimada do IBGE para 2021.`,
+        area:"Área total:10.206,370 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-20.788476, -51.703229",
+        Habitantes:"Quantidade de Habitantes:141.435",
+        estado:"Estado:Mato Grosso do Sul",
+        prefeitura:"https://www.treslagoas.ms.gov.br/"
+    },
     
-imagem = {
-        CampoGrande:'./img/MS/CampoGrande.png', 
-        TresLagoas:`./img/MS/TresLagoas.png`,
-        Aquidauana:`./img/MS/Aquidauana.png`,
-        Dourados:`./img/MS/Dourados.png`,
-        Corumba:`./img/MS/Corumba.png`,
-        Arraias:`./img/TO/Arraias.png`,
-        Palmas:`./img/TO/Palmas.png`,
-        Gurupi:`./img/TO/Gurupi.png`,
-        Angico:`./img/TO/Angico.png`,
-        Almas:`./img/TO/Almas.png`,
-    
-},
-mensagem = {   
-    CampoGrande :`Campo Grande é a capital do estado de Mato Grosso do Sul, 
-    na Região Centro-Oeste do Brasil. Possui clima tropical,
-    com verões quentes e chuvosos e inverno seco.
-    Seu relevo é predominantemente planáltico.`,
-    TresLagoas:`O município de Três Lagoas está localizado na 
-    região leste de Mato Grosso do Sul. A cidade 
-    foi fundada em 15 de junho de 1915 e é 
-    considerada o terceiro município mais 
-    populoso do Estado com aproximadamente 
-    125 mil habitantes segundo população 
-    estimada do IBGE para 2021.`,
-    Aquidauana:`Fundada em 15 de agosto de1892 por 05 pecuaristas,
-    é uma cidade centenária, possui uma arquitetura colonial
-    no Centro Histórico, às margens do rio Aquidauana
-    e entorno da Igreja Matriz, alguns prédios são
-    e outros estão em vias de se tornarem centenários.    `,
-    Dourados:`Dourados é o maior município do interior de Mato Grosso do Sul
-    e está entre os 150 maiores do Brasil, além de ser o 10º maior
-    município da região Centro-Oeste do Brasil. Pela estimativa
-    a cidade de Dourados tem hoje 220 965 habitantes.`,
-    Corumba:`Corumbá localiza-se na região do Pantanal sul-mato-grossense
-    e próxima da fronteira com a Bolívia, à beira do Rio Paraguai.
-    O município é também ponto de parada da ligação ferroviária
-    entre o Brasil e a Bolívia, sendo a última cidade brasileira
-    antes do território boliviano, do qual se separa por fronteira seca`,
-    Arraias:`Seus corpos achatados são compostos de barbatanas peitorais
-    unidas à cabeça e ao tronco com sua infame cauda atrás. Apesar
-    de os olhos da arraia se localizarem no dorso, a boca, as 
-    narinas e as fendas branquiais se localizam na barriga.`,
-    Palmas:`É a mais jovem dentre as capitais brasileiras, tendo sido 
-    fundada em 1989. A cidade é banhada pelo Rio Tocantins,
-    um dos mais importantes do estado. Seu clima é o tropical
-    semiúmido, e o relevo varia de plano a suavemente ondulado,
-    com as maiores elevações situadas a leste. Integra o bioma Cerrado.`,
-    Gurupi:`Gurupi é a terceira maior cidade do Tocantins, sendo o polo
-    regional de todo Sul do estado. As principais fontes de renda do 
-    município são a pecuária e a agricultura, mas o comércio está 
-    em crescimento pujante, aquecendo a economia da cidade.`,
-    Angico:`Hábito: Árvore de grande porte, alcança até 20 m de altura.
-    Caducifólia, característica de locais secos e ensolarados. 
-    Ocorrência: Floresta estacional semidecidual, floresta 
-    ombrófila densa, cerrado, caatinga. No DF, é nativa,
-    surgindo em solos ricos em nutrientes.`,
-    Almas:`Almas é um município da unidade federativa Tocantins.
-    Seu território é composto 100% pelo bioma Cerrado. O IDHM 
-    de Almas é 0,64. O município não possui Política Municipal
-    de Saneamento Básico e possui Plano Municipal de Saneamento Básico.`,
-    
-},
-
-titulo = {
-    CampoGrande:'Campo Grande', 
-    TresLagoas:`Três Lagoas`,
-    Aquidauana:`Aquidauana`,
-    Dourados:`Dourados`,
-    Corumba:`Corumbá`,
-    Arraias:`Arraias`,
-    Palmas:`Palmas`,
-    Gurupi:`Gurupi`,
-    Angico:`Angico`,
-    Almas:`Almas`,
-},
-area = {
-    CampoGrande:'Área total:8.096 km²', 
-    TresLagoas:`Área total:10.206,370 km²`,
-    Aquidauana:`Área total:17.008,5 Km²`,
-    Dourados:`Área total:4.086 km²`,
-    Corumba:`Área total:65.000 km²`,
-    Arraias:`Área total: 5.419,9 km²`,
-    Palmas:`Área total:2.219 km²`,
-    Gurupi:`Área total:1.847 km²`,
-    Angico:`Área total:	438,703 km²`,
-    Almas:`Área total: 4.106,4 km².`,
-},
-LatitudeeLongitude = {
-    CampoGrande:'Latitude e Longitude:-20.463218, -54.613142', 
-    TresLagoas:`Latitude e Longitude:-20.788476, -51.703229`,
-    Aquidauana:`Latitude e Longitude:-20.463001, -55.779558`,
-    Dourados:`Latitude e Longitude:-22.228485, -54.817311`,
-    Corumba:`Latitude e Longitude:-19.020969, -57.648295`,
-    Arraias:`Latitude e Longitude:-12.816049, -47.006613`,
-    Palmas:`Latitude e Longitude:-10.249229, -48.324272`,
-    Gurupi:`Latitude e Longitude:-11.733522, -49.079023`,
-    Angico:`Latitude e Longitude:-6.391276, -47.865895`,
-    Almas:`Latitude e Longitude:-11.441228, -47.224737`,
-},
-Habitantes = {
-    CampoGrande:'Quantidade de Habitantes:954.537', 
-    TresLagoas:`Quantidade de Habitantes:141.435`,
-    Aquidauana:`Quantidade de Habitantes:47 871`,
-    Dourados:`Quantidade de Habitantes:260.640`,
-    Corumba:`Quantidade de Habitantes:99.107 `,
-    Arraias:`Quantidade de Habitantes:10.534`,
-    Palmas:`Quantidade de Habitantes:323 625`,
-    Gurupi:`Quantidade de Habitantes:89 554`,
-    Angico:`Quantidade de Habitantes:3.454`,
-    Almas:`Quantidade de Habitantes:6.979`,
-},
-estado = {
-    CampoGrande:'Estado:Mato Grosso do Sul', 
-    TresLagoas:`Estado:Mato Grosso do Sul`,
-    Aquidauana:`Estado:Mato Grosso do Sul`,
-    Dourados:`Estado:Mato Grosso do Sul`,
-    Corumba:`Estado:Mato Grosso do Sul`,
-    Arraias:`Estado:Tocantins`,
-    Palmas:`Estado:Tocantins`,
-    Gurupi:`Estado:Tocantins`,
-    Angico:`Estado:Tocantins`,
-    Almas:`Estado:Tocantins`,
-},
-prefeitura = {
-    CampoGrande:'https://www.campogrande.ms.gov.br/', 
-    TresLagoas:`https://www.treslagoas.ms.gov.br/`,
-    Aquidauana:`https://www.aquidauana.ms.gov.br/`,
-    Dourados:`https://www.dourados.ms.gov.br/`,
-    Corumba:`https://corumba.ms.gov.br/`,
-    Arraias:`https://arraias.to.gov.br/`,
-    Palmas:`https://www.palmas.to.gov.br/`,
-    Gurupi:`https://gurupi.to.gov.br/`,
-    Angico:`https://www.angico.to.gov.br/`,
-    Almas:`https://www.almas.to.gov.br/`,
-}
-]
+    marker = {
+        latitude: -22.228485,
+        longitude: -54.817311,
+        imagem: "./img/MS/Dourados.png",
+        titulo: "Dourados",
+        descricao:`Dourados é o maior município do interior de Mato Grosso do Sul
+        e está entre os 150 maiores do Brasil, além de ser o 10º maior
+        município da região Centro-Oeste do Brasil. Pela estimativa
+        a cidade de Dourados tem hoje 220 965 habitantes.`,
+        area:"Área total:4.086 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-22.228485, -54.817311",
+        Habitantes:"Quantidade de Habitantes:260.640",
+        estado:"Estado:Mato Grosso do Sul",
+        prefeitura:"https://www.dourados.ms.gov.br/"
+    },
+    marker = {
+        latitude: -19.020969,
+        longitude:  -57.648295,
+        imagem: "./img/MS/Corumba.png",
+        titulo: "Corumbá",
+        descricao:`Corumbá localiza-se na região do Pantanal sul-mato-grossense
+        e próxima da fronteira com a Bolívia, à beira do Rio Paraguai.
+        O município é também ponto de parada da ligação ferroviária
+        entre o Brasil e a Bolívia, sendo a última cidade brasileira
+        antes do território boliviano, do qual se separa por fronteira seca`,
+        area:"Área total:65.000 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-19.020969, -57.648295",
+        Habitantes:"Quantidade de Habitantes:99.107",
+        estado:"Estado:Mato Grosso do Sul",
+        prefeitura:"https://corumba.ms.gov.br/"
+    },
+    marker = {
+        latitude: -12.816049, 
+        longitude: -47.006613,
+        imagem: "./img/TO/Arraias.png",
+        titulo: "Arraias",
+        descricao:`Seus corpos achatados são compostos de barbatanas peitorais
+        unidas à cabeça e ao tronco com sua infame cauda atrás. Apesar
+        de os olhos da arraia se localizarem no dorso, a boca, as 
+        narinas e as fendas branquiais se localizam na barriga.`,
+        area:"Área total: 5.419,9 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-12.816049, -47.006613",
+        Habitantes:"Quantidade de Habitantes:10.534",
+        estado:"Estado:Tocantins",
+        prefeitura:"https://arraias.to.gov.br/"
+    },
+    marker = {
+        latitude: -10.249229, 
+        longitude: -48.324272,
+        imagem: "./img/TO/Palmas.png",
+        titulo: "Palmas",
+        descricao:`É a mais jovem dentre as capitais brasileiras, tendo sido 
+        fundada em 1989. A cidade é banhada pelo Rio Tocantins,
+        um dos mais importantes do estado. Seu clima é o tropical
+        semiúmido, e o relevo varia de plano a suavemente ondulado,
+        com as maiores elevações situadas a leste. Integra o bioma Cerrado.`,
+        area:"Área total:2.219 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-10.249229, -48.324272",
+        Habitantes:"Quantidade de Habitantes:323 625",
+        estado:"Estado:Tocantins",
+        prefeitura:"https://www.palmas.to.gov.br/"
+    },
+    marker = {
+        latitude: -11.733522, 
+        longitude: -49.079023,
+        imagem: "./img/TO/Gurupi.png",
+        titulo: "Gurupi",
+        descricao:`Gurupi é a terceira maior cidade do Tocantins, sendo o polo
+        regional de todo Sul do estado. As principais fontes de renda do 
+        município são a pecuária e a agricultura, mas o comércio está 
+        em crescimento pujante, aquecendo a economia da cidade.`,
+        area:"Área total:1.847 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-11.733522, -49.079023",
+        Habitantes:"Quantidade de Habitantes:89 554",
+        estado:"Estado:Tocantins",
+        prefeitura:"https://gurupi.to.gov.br/"
+    },
+           
+    marker = {
+        latitude: -6.391276, 
+        longitude: -47.865895,
+        imagem: "./img/TO/Angico.png",
+        titulo: "Angico",
+        descricao:`Hábito: Árvore de grande porte, alcança até 20 m de altura.
+        Caducifólia, característica de locais secos e ensolarados. 
+        Ocorrência: Floresta estacional semidecidual, floresta 
+        ombrófila densa, cerrado, caatinga. No DF, é nativa,
+        surgindo em solos ricos em nutrientes.`,
+        area:"Área total:438,703 km²",
+        LatitudeeLongitude:"Latitude e Longitude:-6.391276, -47.865895",
+        Habitantes:"Quantidade de Habitantes:3.454",
+        estado:"Estado:Tocantins",
+        prefeitura:"https://www.angico.to.gov.br/"
+    },
+    marker = {
+        latitude: -11.441228, 
+        longitude: -47.224737,
+        imagem: "./img/TO/Almas.png",
+        titulo: "Almas",
+        descricao:`Almas é um município da unidade federativa Tocantins.
+        Seu território é composto 100% pelo bioma Cerrado. O IDHM 
+        de Almas é 0,64. O município não possui Política Municipal
+        de Saneamento Básico e possui Plano Municipal de Saneamento Básico.`,
+        area:"Área total: 4.106,4 km².",
+        LatitudeeLongitude:"Latitude e Longitude:-11.441228, -47.224737",
+        Habitantes:"Quantidade de Habitantes:6.979",
+        estado:"Estado:Tocantins",
+        prefeitura:"https://www.almas.to.gov.br/"
+    },
+  ]
