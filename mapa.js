e07af072433cdf12fec102c93a89417e53d2ce4a
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-01 19:38:37 -0300

    inicio

--- mapa.js
+++ mapa.js
@@ -1,21 +1,21 @@
-//importação das descricões das cidades
-import {Descricao,imagem,titulo,LatitudeeLongitude,Habitantes,estado,prefeitura,area} from "./mensagens.js";
-
-//Latitude e Longitude centralizado no Brasil
-let map = L.map('map').setView([-12.897489, -49.746094], 5);
-
-//Mapa 
-
+let map = L.map('map').setView([-15.496032, -49.174805], 5);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href=`http://www.openstreetmap.org/copyright`>OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-/*
-
-    Marcadores das cidades do Mato Grosso do Sul: 
-    Campo Grande,
-    Aquidauana,
+//Feito 
+marcadores.forEach((pontos) => {
+    let marker = L.marker([pontos.latitude, pontos.longitude]).addTo(map);
+    let popup = L.popup();
+    marker.bindPopup(`<img src="${pontos.imagem}"><p>${pontos.descricao}</p><p>${pontos.area}</p><p>${pontos.LatitudeeLongitude}</p><p>${pontos.Habitantes}</p><p>${pontos.estado}</p><a href='${pontos.prefeitura}'>Prefeitura local</a>`); 
+});
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
     Três Lagoas,
     Dourados,
     Corumbá
