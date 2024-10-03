503c8dd1a97923dae3d90cc57c2e1674bc750bcb
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-03 13:52:07 -0300

    ta funcionando :)

---
+++ mensagem1.js
@@ -0,0 +1,11 @@
+let marcadores = [
+    marker = {
+     latitude:-12.039321,
+     longitude:-49.394531,
+    },
+    marker = {
+        latitude:-14.039321,
+        longitude:-49.394531,
+    }, 
+    
+]
