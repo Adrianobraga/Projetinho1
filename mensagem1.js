99ab8dbddba9322f27cc124204d92336039977b4
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-03 15:50:04 -0300

    entendi

--- mensagem1.js
+++ mensagem1.js
@@ -1,8 +1,13 @@
 let marcadores = [
     marker = {
-     latitude:-12.039321,
-     longitude:-49.394531,
-     imagem:"./img/MS/Aquidauana.png",
+        latitude: -12.039321,
+        longitude: -49.394531,
+        imagem: "./img/MS/Aquidauana.png",
     },
-    
+    marker = {
+        latitude: -12.039321,
+        longitude: -59.394531,
+        imagem: "./img/MS/Aquidauana.png",
+    }
+
 ]
